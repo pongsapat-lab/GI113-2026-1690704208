b30dbf43876a97ab318c0a6c1b53d719467ac428
pongsapat-lab <pongsapat.piti@bumail.net>
2026-09-10 17:37:21 +0700

    assign01_1

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            const string gameName = "EX3 ShoWDown";
+
+            var char01_name = "Faust";
+            var char01_grade = "C";
+            int char01_lvl = 1;
+            float char01_atk = 10.7f;
+            double char01_def = 5.5;
+            bool isPlayerAble = true;
+
+
+            Console.WriteLine($"");
+            Console.WriteLine($"Character: {char01_name}");
+            Console.WriteLine($"Grade: {char01_grade}");
+            Console.WriteLine($"Level: {char01_lvl}");
+            Console.WriteLine($"Attack: {char01_atk}");
+            Console.WriteLine($"Defence: {char01_def}");
+            Console.WriteLine($"Playable: {isPlayerAble}");
         }
     }
 }
