d850b1a586b492473311baa92fa049922ffd0efc
pongsapat-lab <pongsapat.piti@bumail.net>
2026-09-22 14:43:49 +0700

    lab06

---
+++ Lab06/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Student ID : 1690704208
+ * Name       : Lab06
+ * Section    : 129D
+ * No.        : 
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
