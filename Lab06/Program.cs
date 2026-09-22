6259a6a33fc9f13ab0b378061ea059fd4bceb454
pongsapat-lab <pongsapat.piti@bumail.net>
2026-09-22 16:18:45 +0700

    Update Program.cs

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -56,7 +56,7 @@ namespace Lab06
             //    {
             //        Console.WriteLine("door lock");
             //    }
-                
+
             //}
 
             //else
@@ -75,9 +75,57 @@ namespace Lab06
             //    Console.WriteLine("lived");
             //}
 
+            int pcHp = 100;
+            int targetHp = 20;
+            int attack = 10;
+            int defence = 10;
+            int heal = 2;
+            int target_atk = 15;
 
+            Console.WriteLine("Hero versus Monster");
 
+            Console.WriteLine("Action 1: Attack");
+            Console.WriteLine("Action 2: Defence");
+            Console.WriteLine("Action 3: Heal");
 
+            Console.WriteLine("Choose your action (1-3): ");
+            bool inputAction = int.TryParse(Console.ReadLine(), out int choice);
+
+            if (!inputAction || choice < 1 || choice > 3)
+            {
+                Console.WriteLine("Invalid Value!");
+            }
+            else if (choice == 1)
+            {
+                targetHp -= attack;
+                if (targetHp <= 0)
+                {
+                    Console.WriteLine($"Target take {attack} DMG, Target taking out");
+                }
+                else
+                {
+                    Console.WriteLine($"Target take {attack} DMG, Target has {targetHp} hp left");
+                }
+            }
+
+            else if (choice == 2)
+            {
+                pcHp -= target_atk - defence;
+                if (pcHp <= 0)
+                {
+                    Console.WriteLine($"Player rise guard and take {target_atk} DMG, Player taking out");
+                }
+                else
+                {
+                    Console.WriteLine($"Player rise guard and take {target_atk} DMG, Player has {pcHp} left");
+                }
+
+            }
+            else
+            {
+                pcHp += heal;
+                Console.WriteLine($"Player use heal, now Player has {pcHp} HP left");
+            }
         }
     }
 }
