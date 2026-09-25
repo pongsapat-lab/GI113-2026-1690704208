140efa0a8262e283271ba1874ea9f4da38ce7921
pongsapat-lab <pongsapat.piti@bumail.net>
2026-09-25 12:13:01 +0700

    ass 02

---
+++ Assignment2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Student ID : 1690704208
+ * Name       : Assignment #2
+ * Section    : 129D
+ * No.        : 
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Forge");
+
+            double ironSmelting = 0.25;
+            double ironSalvage = 0.3;
+
+            Console.WriteLine("Please choose you action");
+            Console.WriteLine("Choose 'S' for smelt"); //(Ore -> Ingot)
+            Console.WriteLine("Choose 'B' for breakdown"); //(Ore -> Ingot)
+
+            bool inputAction = int.TryParse(Console.ReadLine(), out int choice);
+
+        }
+    }
+}
